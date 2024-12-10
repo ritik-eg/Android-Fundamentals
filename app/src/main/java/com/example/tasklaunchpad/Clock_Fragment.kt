18b8693a690259ff6a41d88d1deb8d9461285aaf
Ritik <riran@eg.dk>
2024-12-10 15:31:39 +0530

    Launch_Modes

---
+++ app/src/main/java/com/example/tasklaunchpad/Clock_Fragment.kt
@@ -0,0 +1,6 @@
+package com.example.tasklaunchpad
+
+import androidx.fragment.app.Fragment
+
+class Clock_Fragment : Fragment(R.layout.fragment_clock) {
+}
---
+++ app/src/main/java/com/example/tasklaunchpad/FourthActivity.kt
@@ -0,0 +1,15 @@
+package com.example.tasklaunchpad
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+
+class FourthActivity : ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fourth_activity)
+
+    }
+}
+
